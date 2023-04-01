f36423e7911cb3c44c8bde545a0325fd08f7b301
kimovl <mokhov_kyrylo@gmail.com>
2023-04-01 11:00:38 +0300

    Some simple changes for second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Some Changes for this file");
+        System.out.println("third commit");
     }
 }
